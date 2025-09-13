386f6d691b9c9dc8453fe3053575fa0e4081a842
Fl1riX <maksimhripunkov658@gmail.com>
2025-09-13 09:28:41 +0300

    v 0.0.5

--- build.sh
+++ build.sh
@@ -1,33 +1,40 @@
 #!/bin/bash
 set -e
 
-# === Параметры образа ===
-echo "Setting image parameters..."
-IMG=ponos.img
+# === Параметры ===
+BUILD_DIR="build"
+IMG="$BUILD_DIR/ponos.img"
 IMG_SIZE_MB=20
 
-# === Сборка ===
-echo "Building..."
-mkdir -p build
+mkdir -p "$BUILD_DIR"
 
-nasm -f bin mbr_bootloader.asm -o build/mbr.bin
-nasm -f bin boot.asm         -o build/boot.bin
-nasm -f bin kernel.asm       -o build/kernel.bin
+echo "=== Сборка бинарников ==="
+nasm -f bin mbr_bootloader.asm -o "$BUILD_DIR/mbr.bin"
+nasm -f bin boot.asm         -o "$BUILD_DIR/boot.bin"
+nasm -f bin kernel.asm       -o "$BUILD_DIR/kernel.bin"
 
-# === Создаём пустой HDD-образ ===
-echo "Creating an empty HDD image..."
-dd if=/dev/zero of=build/$IMG bs=1M count=$IMG_SIZE_MB
+echo "=== Создание пустого HDD-образа ($IMG_SIZE_MB MB) ==="
+dd if=/dev/zero of="$IMG" bs=1M count=$IMG_SIZE_MB
 
-# === Записываем загрузчик и прочее ===
-echo "Writing the bootloader and other things..."
-dd if=build/mbr.bin    of=build/$IMG conv=notrunc
-dd if=build/boot.bin   of=build/$IMG bs=512 seek=1 conv=notrunc
-dd if=build/kernel.bin of=build/$IMG bs=512 seek=3 conv=notrunc
+echo "=== Запись MBR, boot и kernel в образ ==="
+dd if="$BUILD_DIR/mbr.bin"    of="$IMG" conv=notrunc
+dd if="$BUILD_DIR/boot.bin"   of="$IMG" bs=512 seek=1 conv=notrunc
+dd if="$BUILD_DIR/kernel.bin" of="$IMG" bs=512 seek=3 conv=notrunc
 
-# === Запуск QEMU в режиме HDD с LBA ===
-echo "Running QEMU..."
-qemu-system-i386 \
-    -drive file=build/$IMG,format=raw,if=ide \
-    -boot c \
-    -monitor stdio\
-    -bios bios-256k.bin
+# === Определяем режим запуска ===
+RUN_MODE="$1"   # передаем "debug" для отладки
+
+if [ "$RUN_MODE" = "debug" ]; then
+    echo "=== Запуск QEMU с GDB-сервером ==="
+    qemu-system-i386 \
+        -drive file="$IMG",format=raw,if=ide \
+        -boot c \
+        -s -S     
+else
+    echo "=== Обычный запуск QEMU ==="
+    qemu-system-i386 \
+        -drive file="$IMG",format=raw,if=ide \
+        -boot c
+fi
+
+echo "=== Готово ==="
--- kernel.asm
+++ kernel.asm
@@ -1,40 +1,74 @@
 BITS 16
 ORG 0X1000
 
+;jmp kernel_start 
+
+;%include "calculator.asm"
+
 kernel_start:
   mov ah, 0x00 ; устанавливаем видео режим
   mov al, 0x03 ; текстовый режим 16 текстовый  
 
   int 0x10     ; вызываем прерывание
 
+  mov si, banner
+  call print 
+
   mov si, welcome
   call print
 
-  call read_keyboard
-
-  cli
-  hlt 
+  call read_keyboard 
 
 reboot:
   int 0x19 ; перезапуск системы 
 
-;TODO: повиксить работу с клавиатурой
 read_keyboard:
   mov ah, 0x00
   int 0x16
 
-  cmp al, 8
+  cmp byte [pressed_keys], 254  ; если количество клавиш превышает размер буфера, то происходит перезагрузка
+  jae clear_buffer
+
+  cmp al, 8 ; проверка на backspace
   je .del_character
 
-  mov ah, 0x0E
-  int 0x10     ; выводим один символ из al 
+  cmp al, 13 ; проверка на enter
+  je .enter_pressed
 
-  jne read_keyboard
+  push bx
+  movzx si, [pressed_keys]         
+  mov bx, key_buffer            ; указываем адрес буфера 
+   
+  add bx, si 
+  mov [bx], al                  ; записываем клавишу в буфер + смещение на 1 
+  inc byte [pressed_keys]       ; увеличиваем число нажатых кнопок
+  
+  pop bx
+
+  mov ah, 0x0E      ; функция вывода символа 
+  int 0x10          ; выводим один символ из al 
+
+  jmp read_keyboard ; бесконечный цикл
+
+.enter_pressed:
+  call check_command
+  jmp read_keyboard
 
 .del_character:
   mov ah, 0x03 ; получаем текущую позицию курсора
   mov bh, 0    ; устанавливаем страницу
   int 0x10 
+
+  dec byte [pressed_keys]
+
+  push bx
+  movzx si, [pressed_keys]  ; указываем адрес буфера 
+  mov bx, key_buffer
+   
+  add bx, si 
+  mov byte [bx], 0        ; записываем клавишу в буфер + смещение на 1 
+
+  pop bx
 
   cmp dl, 0    ; проверка на начало строки 
   je read_keyboard
@@ -57,6 +91,157 @@ read_keyboard:
   int 0x10
   jmp read_keyboard ; снова читаем клавиатуру 
 
+clear_buffer:
+  push ax
+  push cx
+  push di 
+
+  mov di, key_buffer
+  mov cx, 254
+  mov al, 0
+  rep stosb  ; rep повтояет инструкцию столько сколько указанно в cx, stosb копирует байт из al по адресу es:di
+
+  pop di
+  pop cx
+  pop ax
+
+  ret 
+
+check_command:  
+  push si ; сохраняем регистры
+  push di 
+  push bx
+
+  movzx si, [pressed_keys]       ; количество нажатых клавиш как счетчик количества клавиш в буфере 
+  mov bx, key_buffer             ; указываем адрес буфера 
+   
+  add bx, si 
+  mov byte [bx], 0              
+
+  ; помощь 
+  mov si, key_buffer
+  mov di, help_cmd
+  call str_cmp
+  jc .help_found
+
+  ; перезагрузка 
+  mov si, key_buffer
+  mov di, reboot_cmd
+  call str_cmp
+  jc .reboot_found
+
+  ; калькулятор
+  mov si, key_buffer
+  mov di, calc_cmd
+  call str_cmp
+  jc .calc_found 
+
+  mov si, key_buffer
+  mov di, clear_cmd 
+  call str_cmp
+  jc .clear_found 
+
+  jnc .not_found  
+
+.not_found:
+  pop bx
+  pop di  
+  pop si
+  call clear_buffer
+  mov byte [pressed_keys], 0  
+
+  mov si, cmd_not_found
+  call print 
+  clc 
+  jmp read_keyboard
+
+.clear_found:
+  pop bx
+  pop di 
+  pop si
+  call clear_buffer
+  mov byte [pressed_keys], 0  
+
+  clc
+  jmp clear_command 
+
+.help_found:
+  pop bx
+  pop di 
+  pop si
+  call clear_buffer
+  mov byte [pressed_keys], 0  
+
+  clc
+  jmp help_command
+  
+.reboot_found:
+  pop bx 
+  pop di 
+  pop si
+  call clear_buffer
+  mov byte [pressed_keys], 0  
+
+  clc
+  jmp reboot
+
+.calc_found:
+  pop bx
+  pop di 
+  pop si
+  call clear_buffer
+  mov byte [pressed_keys], 0  
+
+  clc
+  jmp calc_command
+
+help_command:
+  mov si, help_text
+  call print 
+  jmp read_keyboard 
+
+calc_command:
+  ;call calc
+  ;jmp read_keyboard
+  jmp read_keyboard
+
+clear_command:
+  mov ah, 0x00
+  mov al, 0x03
+
+  int 0x10 
+
+  jmp read_keyboard
+
+str_cmp:
+  push si
+  push di 
+.loop:
+  mov al, [si]
+  inc si
+
+  cmp al, [di] ; посимвольно сравниваем ввод с коммандой 
+  jne .error   ; если имволы не равны вызываем ошибку 
+  inc di       ; выбираем следующий символ из di 
+
+  test al, al  ; проверям на конец строки 
+  jnz .loop
+
+  stc ; устанавливаем СF = 1 
+  
+  pop di
+  pop si
+
+  ret  
+
+.error:
+  pop di
+  pop si
+
+  clc
+  ret 
+
+
 print:
   jmp .loop
 
@@ -72,12 +257,30 @@ print:
   jmp .loop    ; начинаем цикл заново пока не выведется все слово 
 
 .done:
-  ret  
+  ret
 
 ; переменные
+key_buffer times 254 db 0     ; буфер клавиатуры под 256 символов 
+pressed_keys db 0             ; счетчик нажатых клавиш
+
+; команды
+help_cmd db 'help', 0 
+reboot_cmd db 'reboot', 0 
+calc_cmd db 'calc', 0 
+clear_cmd db 'clear', 0 
+help_text db 13, 10,'type:', 13, 10, 'reboot - to reboot the device', 13, 10, 'calc - to open the calculator', 13, 10, 0
 
 ; сообщения 
-welcome db 'Welcome to the Pon operating system!', 13, 10, 'Press "1" to get help, "2" to reboot', 13, 10, 0  ; 13 - возврат каретки в начало строки, 10 - перевод на новую сроку 
-;ent db 'Enter pressed', 13, 10, 0
+welcome db 'Welcome to the Pon operating system!', 13, 10, 'Type "help" to get command list', 13, 10, 0  ; 13 - возврат каретки в начало строки, 10 - перевод на новую сроку 
+banner db 'ooooooooo.                           .oooooo.    .oooooo..o ', 13, 10, \
+          '`888   `Y88.                        d8P`  `Y8b  d8P`    `Y8 ', 13, 10, \
+          ' 888   .d88   .ooooo.  ooo. .oo.   888      888 Y88bo.      ', 13, 10, \
+          ' 888ooo88P   d88  `88b `888P Y88b  888      888  `Y8888o.   ', 13, 10, \
+          ' 888         888   888  888   888  888      888      `Y88b  ', 13, 10, \
+          ' 888         888   888  888   888  `88b    d88  oo     .d8P ', 13, 10, \
+          'o888o        `Y8bod8P  o888o o888o  `Y8bood8P   8``88888P`  ', 13, 10, 13, 10, 0                                                      
+                                                            
+; ошибки
+cmd_not_found db 13, 10, 'Command not found!', 13, 10, 0 
 
-times 1024-($-$$) db 0 
+times 1536-($-$$) db 0 
